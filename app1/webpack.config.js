6024cd8159acd075a5e579914e2761af69d51725
Lucas Carvalho <lucasdecarvalho13@hotmail.com>
2022-01-20 20:16:05 -0300

    webpack

--- app1/webpack.config.js
+++ app1/webpack.config.js
@@ -1,58 +1,68 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const ModuleFederationPlugin = require("webpack").container
-  .ModuleFederationPlugin;
+const ModuleFederationPlugin =
+	require("webpack").container.ModuleFederationPlugin;
 const path = require("path");
 
 module.exports = {
-  entry: "./src/index",
-  mode: "development",
-  devServer: {
-    contentBase: path.join(__dirname, "dist"),
-    historyApiFallback: true,
-    port: 3001,
-    host: "0.0.0.0",
-  },
-  output: {
-    publicPath: "auto",
-    chunkFilename: "[id].[contenthash].js",
-    filename: "[id].[contenthash].js",
-  },
-  resolve: {
-    extensions: [".ts", ".tsx", ".js"],
-  },
-  module: {
-    rules: [
-      {
-        test: /bootstrap\.tsx$/,
-        loader: "bundle-loader",
-        options: {
-          lazy: true,
-        },
-      },
-      {
-        test: /\.tsx?$/,
-        loader: "babel-loader",
-        exclude: /node_modules/,
-        options: {
-          presets: ["@babel/preset-react", "@babel/preset-typescript"],
-        },
-      },
-    ],
-  },
-  plugins: [
-    new ModuleFederationPlugin({
-      name: require("./package.json").name,
-      filename: "remoteEntry.js",
-      exposes: {
-        "./App": "./src/App",
-      },
-      remotes: {
-        providers: "providers@http://localhost:3003/remoteEntry.js",
-      },
-      shared: require("./package.json").dependencies,
-    }),
-    new HtmlWebpackPlugin({
-      template: "./public/index.html",
-    }),
-  ],
+	entry: "./src/index",
+	mode: "development",
+	devServer: {
+		contentBase: path.join(__dirname, "dist"),
+		historyApiFallback: true,
+		port: 3001,
+		host: "0.0.0.0",
+	},
+	output: {
+		publicPath: "auto",
+		chunkFilename: "[id].[contenthash].js",
+		filename: "[id].[contenthash].js",
+	},
+	resolve: {
+		extensions: [".ts", ".tsx", ".js"],
+	},
+	module: {
+		rules: [
+			{
+				test: /bootstrap\.tsx$/,
+				loader: "bundle-loader",
+				options: {
+					lazy: true,
+				},
+			},
+			{
+				test: /\.tsx?$/,
+				loader: "babel-loader",
+				exclude: /node_modules/,
+				options: {
+					presets: ["@babel/preset-react", "@babel/preset-typescript"],
+				},
+			},
+		],
+	},
+	plugins: [
+		new ModuleFederationPlugin({
+			name: require("./package.json").name,
+			filename: "remoteEntry.js",
+			exposes: {
+				"./App": "./src/App",
+			},
+			remotes: {
+				providers: "providers@http://localhost:3003/remoteEntry.js",
+			},
+			shared: {
+				...deps,
+				react: {
+					singleton: true,
+					requiredVersion: deps.react,
+				},
+				"react-dom": {
+					singleton: true,
+					requiredVersion: deps["react-dom"],
+				},
+			},
+		}),
+		new HtmlWebpackPlugin({
+			template: "./public/index.html",
+		}),
+	],
 };
