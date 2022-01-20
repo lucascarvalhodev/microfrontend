479cb484a27236ab343cbafc461e226a8356d7d0
Lucas Carvalho <lcarvalho@zak.app>
2022-01-20 20:58:52 -0300

    webpack

--- app2/src/pages/Page1.tsx
+++ app2/src/pages/Page1.tsx
@@ -1,11 +1,17 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 
-const Page1 = () => (
-  <div>
-    <h3>Page1</h3>
-    <Link to="/page2">Page2</Link>
-  </div>
-);
+const Page1 = () => {
+  const { goBack, push } = useHistory();
+
+  return (
+    <div>
+      <h3>Page1</h3>
+      <Link to="/page2">Page2</Link>
+
+      <button onClick={goBack}>goBack</button>
+    </div>
+  );
+};
 
 export default Page1;
--- orquestrador/webpack.config.js
+++ orquestrador/webpack.config.js
@@ -1,69 +1,70 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin =
-	require("webpack").container.ModuleFederationPlugin;
+  require("webpack").container.ModuleFederationPlugin;
 const path = require("path");
+const deps = require("./package.json").dependencies;
 
 module.exports = {
-	entry: "./src/index",
-	mode: "development",
-	devServer: {
-		contentBase: path.join(__dirname, "dist"),
-		historyApiFallback: true,
-		port: 3000,
-		host: "0.0.0.0",
-	},
-	output: {
-		publicPath: "auto",
-		chunkFilename: "[id].[contenthash].js",
-		filename: "[id].[contenthash].js",
-	},
-	resolve: {
-		extensions: [".ts", ".tsx", ".js"],
-	},
-	module: {
-		rules: [
-			{
-				test: /bootstrap\.tsx$/,
-				loader: "bundle-loader",
-				options: {
-					lazy: true,
-				},
-			},
-			{
-				test: /\.tsx?$/,
-				loader: "babel-loader",
-				exclude: /node_modules/,
-				options: {
-					presets: ["@babel/preset-react", "@babel/preset-typescript"],
-				},
-			},
-		],
-	},
-	plugins: [
-		new ModuleFederationPlugin({
-			name: require("./package.json").name,
-			filename: "remoteEntry.js",
-			remotes: {
-				app1: "app1@http://localhost:3001/remoteEntry.js",
-				app2: "app2@http://localhost:3002/remoteEntry.js",
-				providers: "providers@http://localhost:3003/remoteEntry.js",
-			},
-			shared: {
-				...deps,
-				react: {
-					singleton: true,
-					requiredVersion: deps.react,
-				},
-				"react-dom": {
-					singleton: true,
-					requiredVersion: deps["react-dom"],
-				},
-			},
-		}),
-		new HtmlWebpackPlugin({
-			template: "./public/index.html",
-			filename: "index.html",
-			publicPath: "/",
-		}),
-	],
+  entry: "./src/index",
+  mode: "development",
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    historyApiFallback: true,
+    port: 3000,
+    host: "0.0.0.0",
+  },
+  output: {
+    publicPath: "auto",
+    chunkFilename: "[id].[contenthash].js",
+    filename: "[id].[contenthash].js",
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /bootstrap\.tsx$/,
+        loader: "bundle-loader",
+        options: {
+          lazy: true,
+        },
+      },
+      {
+        test: /\.tsx?$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+        options: {
+          presets: ["@babel/preset-react", "@babel/preset-typescript"],
+        },
+      },
+    ],
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: require("./package.json").name,
+      filename: "remoteEntry.js",
+      remotes: {
+        app1: "app1@http://localhost:3001/remoteEntry.js",
+        app2: "app2@http://localhost:3002/remoteEntry.js",
+        providers: "providers@http://localhost:3003/remoteEntry.js",
+      },
+      shared: {
+        ...deps,
+        react: {
+          singleton: true,
+          requiredVersion: deps.react,
+        },
+        "react-dom": {
+          singleton: true,
+          requiredVersion: deps["react-dom"],
+        },
+      },
+    }),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      filename: "index.html",
+      publicPath: "/",
+    }),
+  ],
 };
